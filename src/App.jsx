2b1572e5923a84379da1706fd2b7e9b2fe02182d
Radhika7019 <radhikahonnappanavara.parvam@gmail.com>
2025-10-18 11:57:25 +0530

    18 th oct commit completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
-import Taskform from "./Components/Taskform";
-import TaskList from "./Components/TaskList";
-import Progresstracker from "./Components/Progresstracker";
 import { useEffect, useState } from "react";
+import ProgressTracker from "./Components/ProgressTracker";
+import TaskForm from "./Components/TaskForm";
+import TaskList from "./Components/TaskList";
+import "./Style.css";
 
-export default function App() {
-  const [tasks, setTasks] = useState([]);
+function App() {
+const[tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    localStorage.setItem
-    ("tasks", JSON.stringify(tasks))
-  });
+useEffect(() => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+});
 
-  const addTask = (task) => {
-    setTasks([...tasks,task]);
-  }
+const addTask = (task) => {
+     setTasks([...tasks, task])
+}
+
+const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+}
+
+const deleteTask = (index) => {
+     setTasks(tasks.filter((_, i) => i != index));
+}
+
+const clearTasks = () => {
+  setTasks([]);
+}
 
   return(
-    <div>
-      <h1>Task Focus</h1>
-      <p>Our friendly TaskManager</p>
-      <Taskform addTask = {addTask}/>
-      <TaskList />
-      <Progresstracker />
-      <button>Clear all tasks</button>
+    <div className="App">
+      <header>
+      <h1 className="title">Task Focus</h1>
+      <p className="tagline">Our Friendly TaskManager</p>
+      </header>
+      <TaskForm addTask={addTask}/>
+      <TaskList tasks = {tasks}
+        updateTask = {updateTask} 
+        deleteTask = {deleteTask}/>
+      <ProgressTracker tasks={tasks}/>
+
+      {tasks.length > 0 &&
+      (<button onClick={clearTasks}>Clear All Tasks</button>)}
     </div>
   )
 }
+
+export default App;
